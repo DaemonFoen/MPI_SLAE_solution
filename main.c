48b689c4d2b219c3e9fa946d9f2ab1ad9ea66cb5
Dmitriy <golenko_dmitriy@mail.ru>
2023-02-15 22:23:42 +0700

    some minor upd

--- main.c
+++ main.c
@@ -13,31 +13,31 @@ double norm (const double * vector, int size){
     return sqrt(norm);
 }
 
-int** separator(int comm_size, int capacity, int** previous_param_array){
-    int** param_array = malloc(sizeof (int*) * 2);
+int** separator(int comm_size, int capacity, int** previous_separation_parameters ){
+    int** separation_parameters = malloc(sizeof (int*) * 2);
 
     if (capacity == N) {
         // Vector separation
         int* len_array = malloc(sizeof (int) * comm_size);
         for (int i = 0; i < comm_size; ++i) {
-            len_array[i] = previous_param_array[0][i]/N;
+            len_array[i] = previous_separation_parameters[0][i] / N;
         }
 
-        int* disp_array = malloc(sizeof(int) * comm_size);
+        int* indent_array = malloc(sizeof(int) * comm_size);
         for (int i = 0; i < comm_size; ++i) {
             if (i == 0) {
-                disp_array[i] = 0;
+                indent_array[i] = 0;
             }
             else{
-                disp_array[i] = disp_array[i - 1] + len_array[i-1];
+                indent_array[i] = indent_array[i - 1] + len_array[i - 1];
             }
         }
-        param_array[0] = len_array;
-        param_array[1] = disp_array;
+        separation_parameters[0] = len_array;
+        separation_parameters[1] = indent_array;
     }
     else {
         // Matrix separation
-        int *len_array = malloc(sizeof(int) * comm_size);
+        int * len_array = malloc(sizeof(int) * comm_size);
         {
             int capacity_copy = capacity;
             int i = 0;
@@ -49,22 +49,28 @@ int** separator(int comm_size, int capacity, int** previous_param_array){
             len_array[i] = capacity_copy;
         }
 
-        int *disp_array = malloc(sizeof(int) * comm_size);
+        int * indent_array = malloc(sizeof(int) * comm_size);
 
         for (int i = 0; i < comm_size; ++i) {
             if (i == 0) {
-                disp_array[i] = 0;
+                indent_array[i] = 0;
             } else {
-                disp_array[i] = disp_array[i - 1] + len_array[i - 1];
+                indent_array[i] = indent_array[i - 1] + len_array[i - 1];
             }
         }
-        param_array[0] = len_array;
-        param_array[1] = disp_array;
+        separation_parameters[0] = len_array;
+        separation_parameters[1] = indent_array;
     }
 
-    return param_array;
+    return separation_parameters;
 }
 
+void separator_free(int** separator){
+    for (int i = 0; i < 2; ++i) {
+        free(separator[i]);
+    }
+    free(separator);
+}
 
 int accuracy (const double * x_next, double * matrix, const double * b, double epsilon){
     int rank;
@@ -75,45 +81,55 @@ int accuracy (const double * x_next, double * matrix, const double * b, double e
 
     int capacity = N*N;
 
-    int** param_array = separator(comm_size,capacity,NULL);
+    int** matrix_separation_parameters = separator(comm_size, capacity, NULL);
 
-    double *rec_array = malloc(sizeof(double) * param_array[0][rank]);
+    double *receiver_array = malloc(sizeof(double) * matrix_separation_parameters[0][rank]);
 
     if (rank == 0) {
-        MPI_Scatterv(matrix, param_array[0], param_array[1], MPI_DOUBLE, rec_array, param_array[0][rank], MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        MPI_Scatterv(matrix, matrix_separation_parameters[0], matrix_separation_parameters[1], MPI_DOUBLE, receiver_array, matrix_separation_parameters[0][rank], MPI_DOUBLE, 0, MPI_COMM_WORLD);
     }
     if (rank != 0){
-        MPI_Scatterv(matrix, param_array[0], param_array[1], MPI_DOUBLE, rec_array, param_array[0][rank], MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        MPI_Scatterv(matrix, matrix_separation_parameters[0], matrix_separation_parameters[1], MPI_DOUBLE, receiver_array, matrix_separation_parameters[0][rank], MPI_DOUBLE, 0, MPI_COMM_WORLD);
     }
 
-    double* y_fragment = malloc(sizeof (double) * (param_array[0][rank]/N));
-    for (int i = 0; i < (param_array[0][rank]/N) ; ++i) {
+    double* y_fragment = malloc(sizeof (double) * (matrix_separation_parameters[0][rank] / N));
+    for (int i = 0; i < (matrix_separation_parameters[0][rank] / N) ; ++i) {
         y_fragment[i] = 0;
     }
 
     //  Ax^n
-    for (int i = 0; i < param_array[0][rank]; ++i) {
-        y_fragment[i/N] += rec_array[i] * x_next[i%N];
+    for (int i = 0; i < matrix_separation_parameters[0][rank]; ++i) {
+        y_fragment[i/N] += receiver_array[i] * x_next[i % N];
     }
 
     //  Ax^n - b
-    for (int i = 0; i < (param_array[0][rank]/N) ; ++i) {
+    for (int i = 0; i < (matrix_separation_parameters[0][rank] / N) ; ++i) {
         y_fragment[i] -= b[i+rank];
     }
 
 
-    int** y_param_array = separator(comm_size, N, param_array);
+    int** y_separation_parameters = separator(comm_size, N, matrix_separation_parameters);
     double * y_n = malloc(sizeof(double ) *N);
-    MPI_Allgatherv(y_fragment,y_param_array[0][rank],MPI_DOUBLE,y_n,y_param_array[0],y_param_array[1],MPI_DOUBLE,MPI_COMM_WORLD);
+    MPI_Allgatherv(y_fragment, y_separation_parameters[0][rank], MPI_DOUBLE, y_n, y_separation_parameters[0], y_separation_parameters[1], MPI_DOUBLE, MPI_COMM_WORLD);
 
     double first_norm = norm(y_n,N);
 
     double second_norm = norm(b,N);
 
     if (first_norm/second_norm < epsilon){
+        free(receiver_array);
+        free(y_fragment);
+        separator_free(matrix_separation_parameters);
+        separator_free(y_separation_parameters);
+        free(y_n);
         return 1;
     }
     else {
+        free(receiver_array);
+        free(y_fragment);
+        separator_free(matrix_separation_parameters);
+        separator_free(y_separation_parameters);
+        free(y_n);
         return 0;
     }
 }
@@ -128,76 +144,76 @@ double * iterative_algorithm(const double * x_previous, double * matrix, const d
     MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
 
     int capacity = N*N; // N*N - num of matrix elements
-    int** param_array = separator(comm_size,capacity,NULL);
-    double *rec_array = malloc(sizeof(double) * param_array[0][rank]);
+    int** matrix_separation_parameters = separator(comm_size, capacity, NULL);
+    double *receiver_array = malloc(sizeof(double) * matrix_separation_parameters[0][rank]);
 
     // matrix send
     if (rank == 0) {
-        MPI_Scatterv(matrix, param_array[0], param_array[1], MPI_DOUBLE, rec_array, param_array[0][rank], MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        MPI_Scatterv(matrix, matrix_separation_parameters[0], matrix_separation_parameters[1], MPI_DOUBLE, receiver_array, matrix_separation_parameters[0][rank], MPI_DOUBLE, 0, MPI_COMM_WORLD);
     }
     if (rank != 0){
-        MPI_Scatterv(matrix, param_array[0], param_array[1], MPI_DOUBLE, rec_array, param_array[0][rank], MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        MPI_Scatterv(matrix, matrix_separation_parameters[0], matrix_separation_parameters[1], MPI_DOUBLE, receiver_array, matrix_separation_parameters[0][rank], MPI_DOUBLE, 0, MPI_COMM_WORLD);
     }
 
-    double* y_fragment = malloc(sizeof (double) * (param_array[0][rank]/N));
-    for (int i = 0; i < (param_array[0][rank]/N) ; ++i) {
+    double* y_fragment = malloc(sizeof (double) * (matrix_separation_parameters[0][rank] / N));
+    for (int i = 0; i < (matrix_separation_parameters[0][rank] / N) ; ++i) {
         y_fragment[i] = 0;
     }
 
     //  Ax^n
-    for (int i = 0; i < param_array[0][rank]; ++i) {
-        y_fragment[i/N] += rec_array[i] * x_previous[i%N];
+    for (int i = 0; i < matrix_separation_parameters[0][rank]; ++i) {
+        y_fragment[i/N] += receiver_array[i] * x_previous[i % N];
     }
 
     //  Ax^n - b
-    for (int i = 0; i < (param_array[0][rank]/N) ; ++i) {
+    for (int i = 0; i < (matrix_separation_parameters[0][rank] / N) ; ++i) {
         y_fragment[i] -= b[i+rank];
     }
 
     // Cuts the vector into parts depending on the number of threads
-    int** y_param_array = separator(comm_size, N, param_array);
+    int** y_separation_parameters = separator(comm_size, N, matrix_separation_parameters);
 
     double * y_n =malloc(sizeof(double ) * N);
 
-    MPI_Allgatherv(y_fragment,y_param_array[0][rank],MPI_DOUBLE,y_n,y_param_array[0],y_param_array[1],MPI_DOUBLE,MPI_COMM_WORLD);
+    MPI_Allgatherv(y_fragment, y_separation_parameters[0][rank], MPI_DOUBLE, y_n, y_separation_parameters[0], y_separation_parameters[1], MPI_DOUBLE, MPI_COMM_WORLD);\
+    MPI_Barrier(MPI_COMM_WORLD);
+    double * matrix_y_fragment = malloc(sizeof(double ) * (matrix_separation_parameters[0][rank] / N));
 
-    double * A_y_fragment = malloc(sizeof(double ) * (param_array[0][rank]/N));
-
-    for (int i = 0; i < param_array[0][rank]; ++i) {
-        A_y_fragment[i/N] += rec_array[i] * y_n[i%N];
+    for (int i = 0; i < matrix_separation_parameters[0][rank]; ++i) {
+        matrix_y_fragment[i / N] += receiver_array[i] * y_n[i % N];
     }
 
-    double fragment_A_y_scalar_mult = 0;
+    double matrix_y_scalar_mult_fragment = 0;
 
-    for (int i = 0; i < param_array[0][rank]/N; ++i) {
-        fragment_A_y_scalar_mult += A_y_fragment[i] * A_y_fragment[i];
+    for (int i = 0; i < matrix_separation_parameters[0][rank] / N; ++i) {
+        matrix_y_scalar_mult_fragment += matrix_y_fragment[i] * matrix_y_fragment[i];
     }
 
-    double fragment_y_A_y_scalar_mult = 0;
+    double y_matrix_y_scalar_mult_fragment = 0;
 
-    for (int i = 0; i < param_array[0][rank]/N; ++i) {
-        fragment_y_A_y_scalar_mult += y_n[i+rank] * A_y_fragment[i];
+    for (int i = 0; i < matrix_separation_parameters[0][rank] / N; ++i) {
+        y_matrix_y_scalar_mult_fragment += y_n[i + rank] * matrix_y_fragment[i];
     }
 
-    double * Ay_Ay_scalar_mult = malloc(sizeof(double ) * comm_size);
+    double * dual_matrix_y_scalar_mult = malloc(sizeof(double ) * comm_size);
 
-    MPI_Gather(&fragment_A_y_scalar_mult,1,MPI_DOUBLE,Ay_Ay_scalar_mult,1,MPI_DOUBLE,0,MPI_COMM_WORLD);
+    MPI_Gather(&matrix_y_scalar_mult_fragment, 1, MPI_DOUBLE, dual_matrix_y_scalar_mult, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-    double * y_Ay_scalar_mult = malloc(sizeof(double ) * comm_size);
+    double * y_matrix_y_scalar_mult = malloc(sizeof(double ) * comm_size);
 
-    MPI_Gather(&fragment_y_A_y_scalar_mult,1,MPI_DOUBLE,y_Ay_scalar_mult,1,MPI_DOUBLE,0,MPI_COMM_WORLD);
-
+    MPI_Gather(&y_matrix_y_scalar_mult_fragment, 1, MPI_DOUBLE, y_matrix_y_scalar_mult, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Barrier(MPI_COMM_WORLD);
     double t;
     if ( rank == 0 ) {
-        double y_Ay_sum = 0;
-        double Ay_sum = 0;
+        double y_matrix_y_sum = 0;
+        double matrix_y_sum = 0;
         for (int i = 0; i < comm_size; ++i) {
-            y_Ay_sum += y_Ay_scalar_mult[i];
+            y_matrix_y_sum += y_matrix_y_scalar_mult[i];
         }
         for (int i = 0; i < comm_size; ++i) {
-            Ay_sum += Ay_Ay_scalar_mult[i];
+            matrix_y_sum += dual_matrix_y_scalar_mult[i];
         }
-        t = (double)(y_Ay_sum / Ay_sum);
+        t = (double)(y_matrix_y_sum / matrix_y_sum);
 
         MPI_Bcast(&t,1,MPI_DOUBLE,0,MPI_COMM_WORLD);
     }
@@ -207,18 +223,27 @@ double * iterative_algorithm(const double * x_previous, double * matrix, const d
 
 
 
-    double* x_next_fragment = malloc(sizeof(double ) * (param_array[0][rank]/N));
+    double* x_next_fragment = malloc(sizeof(double ) * (matrix_separation_parameters[0][rank] / N));
 
-    for (int i = 0; i < param_array[0][rank]/N; ++i) {
+    for (int i = 0; i < matrix_separation_parameters[0][rank] / N; ++i) {
         x_next_fragment[i] += x_previous[i+rank] - (t*y_n[i+rank]);
     }
 
     double * x_next = malloc(sizeof(double )*N);
 
-    int** x_param_array = separator(comm_size,N,param_array);
-
-    MPI_Allgatherv(x_next_fragment,param_array[0][rank]/N,MPI_DOUBLE,x_next,x_param_array[0],x_param_array[1],MPI_DOUBLE,MPI_COMM_WORLD);
-
+    int** x_separation_parameters = separator(comm_size, N, matrix_separation_parameters);
+    MPI_Allgatherv(x_next_fragment, matrix_separation_parameters[0][rank] / N, MPI_DOUBLE, x_next, x_separation_parameters[0], x_separation_parameters[1], MPI_DOUBLE, MPI_COMM_WORLD);
+    MPI_Barrier(MPI_COMM_WORLD);
+    free(receiver_array);
+    free(y_fragment);
+    free(y_n);
+    free(matrix_y_fragment);
+    free(dual_matrix_y_scalar_mult);
+    free(y_matrix_y_scalar_mult);
+    free(x_next_fragment);
+//    separator_free(x_separation_parameters);
+//    separator_free(y_separation_parameters);
+//    separator_free(matrix_separation_parameters);
     return x_next;
 }
 
@@ -232,6 +257,7 @@ int main() {
     double * x;
     double * b;
     double * matrix;
+
     x = malloc(sizeof(double) * N);
     b = malloc(sizeof(double) * N);
     matrix = malloc(sizeof(double) * capacity);
@@ -251,7 +277,7 @@ int main() {
         b[i] = (15 + i);
     }
 
-
+    // Flag for checking the accuracy of calculated values
     int flag = 0;
     int count = 0;
     while (flag == 0){
@@ -268,8 +294,11 @@ int main() {
         }
     }
     if (rank == 0){
-        printf("num of iteration %d\n", count);
+        printf("\nnum of iteration %d\n", count);
     }
+    free(x);
+    free(b);
+    free(matrix);
     MPI_Finalize();
     return 0;
 }
